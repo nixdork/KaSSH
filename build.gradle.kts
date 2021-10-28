e53f1721eedab4292236292db5a111ddff074808
Jonas Gorauskas <jgorauskas@gmail.com>
2021-10-27 18:34:18 -0700

    project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "org.nixdork"
+version = "0.1.0-alpha"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
